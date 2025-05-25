ac2cb17dae17cc0be095e5eaf50a63ecb22686d2
jin <crispy4343@gmail.com>
2025-05-25 14:34:14 +0900

    reactRouterDevTools 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { reactRouterDevTools } from 'react-router-devtools';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -7,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    reactRouterDevTools(),
     reactRouter(),
     tsconfigPaths(),
     svgr({
