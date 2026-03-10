13430ba32e2e90518788c125efbb1a0a10ec0966
jin <crispy4343@gmail.com>
2026-03-11 08:20:21 +0900

    HandledError 타입 추가: handleServerError 함수의 일관된 에러 응답 타입 정의

--- app/common/types/common.types.ts
+++ app/common/types/common.types.ts
@@ -3,3 +3,6 @@ export interface ServerException {
   message?: string; // 에러 메시지
   path?: string; // 에러 발생 경로
 }
+
+// * handleServerError 함수에서 예외처리되는 일관된 에러 응답 타입
+export type HandledError = { error: ServerException };
